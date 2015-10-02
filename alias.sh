1178922d08ae1495b485024d67e023a399be90b6
Steven Koch <stvkoch@gmail.com>
2015-10-02 10:33:12 +0100

    create a simple ZSH configuration

---
+++ alias.sh
@@ -0,0 +1,11 @@
+
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xset --clipboard ---output'
+
+alias _='sudo'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias G='grep'
+
+
+source ~/.alias.sh
---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+
+source ~/.zsh_custom/zsh-git-prompt/zshrc.sh
+
+PROMPT='%B%m%~%b$(git_super_status) %# '
+export PATH="${PATH}:~/bin"
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
